0ac31a1c278d28cb873cbd066a5f4e57dbddb941
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-03-27 11:17:30 +0300

    Add some style to the CartButton

--- Components/CartButton/index.js
+++ Components/CartButton/index.js
@@ -7,6 +7,7 @@ class CartButton extends Component {
   render() {
     return (
       <Icon
+        style={{ right: 10, color: "#FFF" }}
         name="cart"
         onPress={() => this.props.navigation.navigate("CoffeeCart")}
       />
